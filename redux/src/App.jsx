b3bd2969f0be83e88e14721b821f128017b0cf6e
sree0077 <sreerajkasaragod007@gmail.com>
2025-10-07 11:31:46 +0530

    Add redux project files properly

---
+++ redux/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+function App() {
+  return (
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        <CreatePost></CreatePost>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
